a4e1825ee5db8731184315831adce51e0259b1ea
Gopal <gopalakrishnan.tr@gmail.com>
2017-02-02 15:21:31 -0500

    message database

---
+++ demo-db.sql
@@ -0,0 +1,58 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.17-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for demo
+DROP DATABASE IF EXISTS `demo`;
+CREATE DATABASE IF NOT EXISTS `demo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `demo`;
+
+
+-- Dumping structure for table demo.message
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(50) NOT NULL,
+  `read` char(1) NOT NULL,
+  `user-id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_message_user` (`user-id`),
+  CONSTRAINT `fk_message_user` FOREIGN KEY (`user-id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table demo.message: ~0 rows (approximately)
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` (`id`, `text`, `read`, `user-id`) VALUES
+	(1, 'message1', 'N', 1),
+	(2, 'message2', 'N', 1),
+	(3, 'message1.1', 'N', 2),
+	(4, 'message1.2', 'N', 2),
+	(5, 'message1.3', 'N', 2);
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+
+
+-- Dumping structure for table demo.user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table demo.user: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `name`) VALUES
+	(1, 'GOPAL'),
+	(2, 'MIKE');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
